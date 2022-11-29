c7783af9079baaf52b022d72cff8a40eb7b0cbc0
BuddyBoyVilla <74051918+BuddyBoyVilla69@users.noreply.github.com>
2022-11-29 20:16:53 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+version '1.0'
+author 'BuddyBoyVilla#7679'
+description 'BuddyBoyVilla#7679'
+
+client_scripts {
+    'cl_practice.lua'
+}
+server_scripts {
+    'sv_practice.lua'
+}
+
+game 'common'
+fx_version 'adamant'
+
